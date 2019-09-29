3ec6d529f924b10416e6c4fc9b74c82794f6dbd0
Andreas Gimpel <an.gimpel@gmx.de>
2019-09-29 17:46:35 +0200

    added landing page

--- apps/basic/views.py
+++ apps/basic/views.py
@@ -4,7 +4,31 @@ from django.shortcuts import render
 def index(request):
 
     context = {
-        'title': 'Home'
+        'title': 'Home',
+        'nav_title': [['home', None]],
     }
 
     return render(request, 'basic/index.html', context=context)
+
+
+
+def about(request):
+
+    context = {
+        'title': 'About',
+        'nav_title': [['about', None]],
+    }
+
+    return render(request, 'basic/about.html', context=context)
+
+
+
+
+def contact(request):
+
+    context = {
+        'title': 'Contact',
+        'nav_title': [['contact', None]],
+    }
+
+    return render(request, 'basic/contact.html', context=context)
